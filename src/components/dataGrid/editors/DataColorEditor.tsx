f85b5978f684af471e16eba18f3cd945d70cddf4
Arthur Lisek-Koper <arthurlisekkoper@gmail.com>
2025-03-29 10:15:22 -0500

    Add styling to icon data editor

--- src/components/dataGrid/editors/DataColorEditor.tsx
+++ src/components/dataGrid/editors/DataColorEditor.tsx
@@ -1,3 +1,4 @@
+import DynamicIcon from "@/components/icons/DynamicIcon";
 import { Icon } from "@/types/data/icon";
 import { Button } from "@chakra-ui/react";
 import { JSX } from "react";
@@ -9,7 +10,10 @@ type DataColorEditorProps = {
 
 const DataColorEditor = ({ icon, onClick }: DataColorEditorProps): JSX.Element => {
   return (
-    <Button onClick={onClick}>{icon?.id} {icon?.color}
+    <Button onClick={onClick} variant={"ghost"} maxHeight={"2rem"}>
+      {icon && (
+        <DynamicIcon iconId={icon.id} color={icon.color} />
+      )}
     </Button>
   );
 };
